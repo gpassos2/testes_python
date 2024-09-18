a3434933ea4dceab905ba61105e118d9483a31b6
gpassos2 <gpassos19892@gmail.com>
2024-09-17 23:05:16 -0300

    adicionando historico de respostas com um arquivo csv

--- reclamacao.py
+++ reclamacao.py
@@ -1,11 +1,15 @@
 import streamlit as st
+import pandas as pd
 
-
-reclamacao = "sem reclamações ainda"
 
 st.title("Um novo lugar para vc depositar todas as suas reclamações. Aproveite.")
 
 reclamacao = str(st.chat_input("digite aqui a sua reclamação" ))
 
+with open('respostas.csv', 'a') as respostas:
+    respostas.write(f'\n{reclamacao}')
+
+
 with st.chat_message("user"):
-    st.write(reclamacao)
+    df = pd.read_csv("respostas.csv")
+    st.dataframe(df)
