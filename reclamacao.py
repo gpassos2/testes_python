f417bb695ae827525046ea58a36436167fce941e
gpassos2 <gpassos19892@gmail.com>
2024-09-17 23:22:01 -0300

    mudando de chat input para text input

--- reclamacao.py
+++ reclamacao.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 st.title("Um novo lugar para vc depositar todas as suas reclamações. Aproveite.")
 
-reclamacao = str(st.chat_input("digite aqui a sua reclamação" ))
+reclamacao = st.text_input("digite aqui a sua reclamação" )
 
 with open('respostas.csv', 'a') as respostas:
     respostas.write(f'\n{reclamacao}')
